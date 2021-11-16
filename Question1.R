da25dfc657b86afb2f944bf8f5aac176b3c0213f
Matthew Lanum <matthewlanum@gmail.com>
2021-11-16 00:04:07 -0500

    part 1 b

--- Question1.R
+++ Question1.R
@@ -1,9 +1,14 @@
 #Load packages
 
-x <- 90
-
-TJ <- "cool"
-
 
 #STEP 1
-df <- read.csv('')
+#a)
+df <- read.csv('Titanic_updated.csv', stringsAsFactors = TRUE)
+df[ ,c(1,2,7,8)] <- lapply(df[,c(1,2,7,8)], factor)
+str(df)
+
+#b)
+lapply(lapply(df,is.na), sum)
+df$Age[is.na(df$Age)] <- median(df$Age, na.rm = TRUE)
+
+#STEP 2
